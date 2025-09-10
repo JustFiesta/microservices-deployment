e664a1a06bc9993de93acfc6e03afc6f662f3859
JustFiesta <mateusz.bocak9@gmail.com>
2025-09-10 16:15:45 +0200

    Test dev resources to check TF connectivity

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "mbocak-kubernetes-tf-state-bucket"
+    key            = "state/dev/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+  }
+}
