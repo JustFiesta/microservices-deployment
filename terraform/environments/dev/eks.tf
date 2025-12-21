f36591eb8fc727894434eb6d501d10bd810349a5
JustFiesta <mateusz.bocak9@gmail.com>
2025-12-21 18:26:31 +0100

    Add gp3 storage class for EKS and update Prometheus storage configuration

--- terraform/environments/dev/eks.tf
+++ terraform/environments/dev/eks.tf
@@ -32,3 +32,22 @@ module "eks" {
 
   tags = local.tags
 }
+
+#
+# Storage Classes for EKS
+#
+resource "kubernetes_storage_class" "gp3" {
+  metadata {
+    name = "gp3"
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    type   = "gp3"
+    fsType = "ext4"
+  }
+}
