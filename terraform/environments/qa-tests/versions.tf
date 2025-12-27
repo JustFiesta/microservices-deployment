303b77516bb12a35f0bc1d0875d49d66437bb599
JustFiesta <mateusz.bocak9@gmail.com>
2025-12-27 12:57:47 +0100

    Create copy of dev env to test flow of adding new one

---
+++ terraform/environments/qa-tests/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  required_version = ">= 1.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
+    }
+  }
+}
