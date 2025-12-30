8977f82730e4c258b56bbdf3babca27f1777cc9e
JustFiesta <mateusz.bocak9@gmail.com>
2025-12-30 17:06:38 +0100

    Remove qa-tests environment

--- terraform/environments/qa-tests/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mbocak-kubernetes-tf-state-bucket"
-    key            = "state/qa-tests/terraform.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
-  }
-}
